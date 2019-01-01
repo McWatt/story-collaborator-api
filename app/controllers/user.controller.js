8b6819536687eb364b9666cd9ab86464fd62c3b6
Erik Phipps <erik.phipps@gmail.com>
2019-01-01 10:49:39 -0800

    adding prettier pre-commit formatting support

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,6 +1,6 @@
-const User = require('../models/user.model.js');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const User = require("../models/user.model.js");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 exports.register = function(req, res) {
   var newUser = new User(req.body);
@@ -17,147 +17,134 @@ exports.register = function(req, res) {
   });
 };
 
-// exports.create = (req, res) => {
-//     if (!req.body.name) {
-//         return res.status(400).send({
-//             message: 'User name cannot be empty'
-//         })
-//     }
-
-//     if (!req.body.email) {
-//         return res.status(400).send({
-//             message: 'User email cannot be empty'
-//         })
-//     }
-
-//     const user = new User({
-//         name: req.body.name || "Untitled User",
-//         email: req.body.email
-//     });
-
-//     user.save()
-//         .then(data => {
-//             res.send(data);
-//         }).catch(err => {
-//             res.status(500).send({
-//                 message: err.message || 'Some error occurred while creating the user.'
-//             })
-//         })
-// };
-
 exports.findAll = (req, res) => {
-    User.find()
-        .then(users => {
-            res.send(users);
-        }).catch(err => {
-            res.status(500).send({
-                message: err.message || 'Some error occurred while retrieving users.'
-            });
-        });
+  User.find()
+    .then(users => {
+      res.send(users);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving users."
+      });
+    });
 };
 
 exports.findOne = (req, res) => {
-    User.findById(req.params.userId)
-        .then(user => {
-            if (!user) {
-                return res.status(404).send({
-                    message: 'User not found with id ' + req.params.userId
-                });
-            }
-            res.send(user);
-        }).catch(err => {
-            if (err.kind === 'ObjectId') {
-                return res.status(404).send({
-                    message: 'User not found with id ' + req.params.userId
-                });
-            }
-
-            return res.status(500).send({
-                message: 'Error retrieving user with id ' + req.params.userId
-            });
+  User.findById(req.params.userId)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send({
+          message: "User not found with id " + req.params.userId
         });
+      }
+      res.send(user);
+    })
+    .catch(err => {
+      if (err.kind === "ObjectId") {
+        return res.status(404).send({
+          message: "User not found with id " + req.params.userId
+        });
+      }
+
+      return res.status(500).send({
+        message: "Error retrieving user with id " + req.params.userId
+      });
+    });
 };
 
 // Update a user identified by the userId in the request
 exports.update = (req, res) => {
-    // Validate Request
-    if (!req.body) {
-        return res.status(400).send({
-            message: "User body can not be empty"
-        });
-    }
-    
-    if (typeof req.body !== 'object') {
-        return res.status(400).send({
-            message: "User body must be an object"
-        });
-    }
+  // Validate Request
+  if (!req.body) {
+    return res.status(400).send({
+      message: "User body can not be empty"
+    });
+  }
 
-    // Find user and update it with the request body
-    User.findByIdAndUpdate(req.params.userId, req.body,
-        { new: true })
-        .then(user => {
-            if (!user) {
-                return res.status(404).send({
-                    message: "User not found with id " + req.params.userId
-                });
-            }
-            res.send(user);
-        }).catch(err => {
-            if (err.kind === 'ObjectId') {
-                return res.status(404).send({
-                    message: "User not found with id " + req.params.userId
-                });
-            }
-            return res.status(500).send({
-                message: "Error updating user with id " + req.params.userId
-            });
+  if (typeof req.body !== "object") {
+    return res.status(400).send({
+      message: "User body must be an object"
+    });
+  }
+
+  // Find user and update it with the request body
+  User.findByIdAndUpdate(req.params.userId, req.body, { new: true })
+    .then(user => {
+      if (!user) {
+        return res.status(404).send({
+          message: "User not found with id " + req.params.userId
         });
+      }
+      res.send(user);
+    })
+    .catch(err => {
+      if (err.kind === "ObjectId") {
+        return res.status(404).send({
+          message: "User not found with id " + req.params.userId
+        });
+      }
+      return res.status(500).send({
+        message: "Error updating user with id " + req.params.userId
+      });
+    });
 };
 
 // Delete a user with the specified userId in the request
 exports.delete = (req, res) => {
-    User.findByIdAndRemove(req.params.userId)
-        .then(user => {
-            if (!user) {
-                return res.status(404).send({
-                    message: "User not found with id " + req.params.userId
-                });
-            }
-            res.send({ message: "User deleted successfully!" });
-        }).catch(err => {
-            if (err.kind === 'ObjectId' || err.name === 'NotFound') {
-                return res.status(404).send({
-                    message: "User not found with id " + req.params.userId
-                });
-            }
-            return res.status(500).send({
-                message: "Could not delete user with id " + req.params.userId
-            });
+  User.findByIdAndRemove(req.params.userId)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send({
+          message: "User not found with id " + req.params.userId
         });
+      }
+      res.send({ message: "User deleted successfully!" });
+    })
+    .catch(err => {
+      if (err.kind === "ObjectId" || err.name === "NotFound") {
+        return res.status(404).send({
+          message: "User not found with id " + req.params.userId
+        });
+      }
+      return res.status(500).send({
+        message: "Could not delete user with id " + req.params.userId
+      });
+    });
 };
 
 exports.signIn = function(req, res) {
-    User.findOne({
-        email: req.body.email
-    }, function (err, user) {
-        if (err) throw err;
-        if (!user) {
-            res.status(401).json({ message: 'Authentication failed. User not found.' });
-        } else if (user) {
-            if (!user.comparePassword(req.body.password)) {
-                res.status(401).json({ message: 'Authentication failed. Wrong password.' });
-            } else {
-                return res.json({ token: jwt.sign({ email: user.email, fullName: user.fullName, _id: user._id }, 'RESTFULAPIs') });
-            }
+  User.findOne(
+    {
+      email: req.body.email
+    },
+    function(err, user) {
+      if (err) throw err;
+      if (!user) {
+        res
+          .status(401)
+          .json({ message: "Authentication failed. User not found." });
+      } else if (user) {
+        if (!user.comparePassword(req.body.password)) {
+          res
+            .status(401)
+            .json({ message: "Authentication failed. Wrong password." });
+        } else {
+          return res.json({
+            token: jwt.sign(
+              { email: user.email, fullName: user.fullName, _id: user._id },
+              "RESTFULAPIs"
+            )
+          });
         }
-    });
-}
+      }
+    }
+  );
+};
 
 exports.loginRequired = function(req, res, next) {
-    if (req.user) {
-        next();
-    } else {
-        return res.status(401).json({ message: 'Unauthorized user!' });
-    }
-}
+  if (req.user) {
+    next();
+  } else {
+    return res.status(401).json({ message: "Unauthorized user!" });
+  }
+};
